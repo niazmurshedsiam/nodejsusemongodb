1291414af77fa06263f7a000b73d63d34284e09e
niazmurshedsiam <niazmurshed16@gmail.com>
2021-03-26 21:04:15 +0600

    Save data in database and Read all data

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:false}));
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://organicUser:99qNR2tFMW1U5pzh@cluster0.bm8mo.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true,useUnifiedTopology: true });
@@ -7,16 +10,15 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 client.connect(err => {
-  const collection = client.db("organicdb").collection("products");
-  // perform actions on the collection object
-  
-  const product = {name: "modhu", price: 25, quantity: 20};
-  collection.insertOne(product)
-  .then(result =>{
-    console.log('one product added');
+  const productCollection = client.db("organicdb").collection("products");
+  app.post("/addProduct",(req,res)=>{
+    const product = req.body;
+    productCollection.insertOne(product)
+    .then(result =>{
+      console.log('data added successfully');
+      res.send('success');
+    })
   })
-  console.log('Database connect');
-  
 });
 
 app.listen(5000);
